7d9bbd5eab702ccc13c1a7917223869914363d7d
Shubham Patil <68894316+shub6059@users.noreply.github.com>
2024-04-02 12:38:20 +0530

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const port = 3000;
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/html'); // Change content type to text/html
-    res.write('<html><head><title>Welcome to the World of Cloud</title>');
+    res.write('<html><head><title>Welcome to the World of Cloud shubham</title>');
     res.write('<style>body { font-family: Arial, sans-serif; color: #007bff; }</style>'); // Add CSS for font and color
     res.write('</head><body>');
     res.write('<h1>Welcome to the World of Cloud Shubham Patil !!!</h1>'); // Your content
